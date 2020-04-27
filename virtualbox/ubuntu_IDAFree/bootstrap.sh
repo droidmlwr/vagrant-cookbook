ab4d30bc1ee855fe901aeb246540e99a394844ab
sp1cycoffee <shrimptankzilla@gmail.com>
2020-04-27 08:55:24 -0400

    rename staticAnalysis box to IDAFree

---
+++ virtualbox/ubuntu_IDAFree/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apt-get update
+
+
+apt-get install -y [service]
+apt-get install -y [service]
+apt-get install -y [service]
+apt-get install -y [service]
+apt-get install -y [service]
+apt-get install -y [service]
+apt-get install -y [service]
+apt-get install -y [service]
+apt-get install -y [service]
+apt-get install -y [service]
+apt-get install -y [service]
