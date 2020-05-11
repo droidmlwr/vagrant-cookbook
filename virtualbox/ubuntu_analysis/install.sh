8e013d844ec59f1289e522f11e3eb65f64437570
Emily Eubanks <shrimptankzilla@gmail.com>
2020-05-10 21:06:34 -0400

    Create install.sh

---
+++ virtualbox/ubuntu_analysis/install.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+apt-get -y upgrade
+
+apt-get -y install libffi-dev
+apt-get -y install libfuzzy-dev
+apt-get -y install net-tools
+
+apt-get -y install curl
+apt-get -y install vim
+
+apt-get -y install python
+apt-get -y install python-dev
+
+apt-get -y install perl
+apt-get -y install libnet-server-perl
+apt-get install -y libnet-dns-perl
+apt-get install -y libipc-shareable-perl
+apt-get install -y libdigest-sha-perl
+apt-get install -y libio-socket-ssl-perl
+
+sudo su
+groupadd inetsim
+sudo su vagrant
+apt-get install inetsim
+
+# sudo su
+# echo "deb http://www.inetsim.org/debian/ binary/" > /etc/apt/sources.list.d/inetsim.list
+# wget -O - http://www.inetsim.org/inetsim-archive-signing-key.asc | apt-key add -
+# apt update
+# apt-get install inetsim
